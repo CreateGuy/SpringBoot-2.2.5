7bfbc216abd2ba1ac5b270e70a6f6a637efdac05
帆布 <17623250093@163.com>
2022-10-10 23:10:46 +0800

    2

---
+++ spring-boot-project/demo/src/main/java/org/lzx/springBootDemo/DemoApplication.java
@@ -0,0 +1,15 @@
+package org.lzx.springBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuzhixuan
+ * @date 2022-10-10
+ */
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class);
+	}
+}
